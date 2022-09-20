903d557e023b78651f83e2d93fcf1455e4b6ca37
thisisforeign <danneil_teng@yahoo.com.sg>
2022-09-20 14:35:56 +0800

    Comment out those that I'm having difficulty with implementing. Will come back to solve

--- script.js
+++ script.js
@@ -1,40 +1,76 @@
-const container = document.querySelector("#container");
+const board = document.querySelector("#container");
 const slider = document.getElementById("myRange");
+const button = document.querySelector("button");
+const size = document.querySelector("#size");
+const resetBtn = document.querySelector("#reset");
+/*const rainbowBtn = document.querySelector("#rgb");
+const eraserBtn = document.querySelector("#eraser");*/
 
 function createGrid(length) {
   let area = length * length;
+  board.style.gridTemplateColumns = `repeat(${length}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${length}, 1fr)`;
+
   for (let i = 0; i < area; i++) {
-    const div1 = document.createElement("div");
+    let div1 = document.createElement("div");
     div1.classList.add("box");
-    container.appendChild(div1);
+    board.appendChild(div1);
     div1.style.border = "1px solid green";
-    container.style.gridTemplateColumns = `repeat(${length}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${length}, 1fr)`;
+    div1.addEventListener("mouseover", black);
   }
-  let gridPixels = container.querySelectorAll("div");
-  gridPixels.forEach((gridPixel) =>
-    gridPixel.addEventListener("mouseover", highlighted)
-  );
 }
 
-function highlighted() {
+function black() {
   const boxes = document.querySelectorAll(".box");
   boxes.forEach((box) =>
     box.addEventListener("mouseover", () => {
-      box.style.cssText = "background-color: black;";
-      console.log("colored");
+      box.style.background = "black";
     })
   );
+  console.log("black");
 }
-highlighted();
+
+/*function rainbow() {
+  let red = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  console.log(`${red}, ${blue}, ${green}`);
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) =>
+    box.addEventListener("mouseover", () => {
+      box.style.background = `rgb(${red}, ${blue}, ${green})`;
+    })
+  );
+  console.log("rainbow");
+}
+
+function eraser() {
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) =>
+    box.addEventListener("mouseover", () => {
+      box.style.background = "background-color: white;";
+    })
+  );
+  console.log("erased");
+}*/
 
 function replaceGrid() {
-  let grid = container.querySelectorAll("div");
+  let grid = board.querySelectorAll("div");
   grid.forEach((grids) => grids.remove());
+  size.innerHTML = `${slider.value} x ${slider.value}`;
   createGrid(slider.value);
-  console.log(slider.value);
 }
 
-createGrid(4);
+function startUp() {
+  createGrid(8);
+  size.innerHTML = `${slider.value} x ${slider.value}`;
+  console.log(size.innerHTML);
+}
+startUp();
 
 slider.addEventListener("mouseup", replaceGrid);
+slider.addEventListener("mousemove", () => {
+  size.innerHTML = `${slider.value} x ${slider.value}`;
+});
+resetBtn.addEventListener("click", replaceGrid);
+//eraserBtn.addEventListener("click", eraser);
